a0b3693f60d0ee0cdb67318a50c757af70212bfb
julie <jcortese@ufl.edu>
2022-12-11 16:18:12 -0500

    got background color (and main menu) to display in pygame window

--- town_tune.py
+++ town_tune.py
@@ -1,53 +1,53 @@
+import sys
 import pygame
 from constants import *
 
 
 def draw_start_menu(screen):
     # Initialize title font
-    start_title_font = pygame.font.Font(None, 100)
+    start_title_font = pygame.font.Font(None, 60)
     button_font = pygame.font.Font(None, 70)
     # Color background
     screen.fill(BG_COLOR)
 
     # Initialize and draw title
-    title_surface = start_title_font.render("Sudoku", 0, LINE_COLOR)
+    title_surface = start_title_font.render("Animal Crossing Town Tune", 0, LINE_COLOR)
     title_rectangle = title_surface.get_rect(
         center=(WIDTH // 2, HEIGHT // 2 - 150))
     screen.blit(title_surface, title_rectangle)
 
     # Initialize buttons
     # Initialize text first
-    easy_text = button_font.render("Easy", 0, (255, 255, 255))
-    med_text = button_font.render("Medium", 0, (255, 255, 255))
-    hard_text = button_font.render("Hard", 0, (255, 255, 255))
+    start_text = button_font.render("Start", 0, (255, 255, 255))
+    quit_text = button_font.render("Quit", 0, (255, 255, 255))
 
     # Initialize button background color and text
-    easy_surface = pygame.Surface((easy_text.get_size()[0] + 20, easy_text.get_size()[1] + 20))
+    easy_surface = pygame.Surface((start_text.get_size()[0] + 20, start_text.get_size()[1] + 20))
     easy_surface.fill(LINE_COLOR)
-    easy_surface.blit(easy_text, (10, 10))
+    easy_surface.blit(start_text, (10, 10))
 
-    med_surface = pygame.Surface((med_text.get_size()[0] + 20, med_text.get_size()[1] + 20))
+    med_surface = pygame.Surface((quit_text.get_size()[0] + 20, quit_text.get_size()[1] + 20))
     med_surface.fill(LINE_COLOR)
-    med_surface.blit(med_text, (10, 10))
-
-    hard_surface = pygame.Surface((hard_text.get_size()[0] + 20, hard_text.get_size()[1] + 20))
-    hard_surface.fill(LINE_COLOR)
-    hard_surface.blit(hard_text, (10, 10))
+    med_surface.blit(quit_text, (10, 10))
 
     # Initialize button rectangle
     easy_rectangle = easy_surface.get_rect(
         center=(WIDTH // 2, HEIGHT // 2 + 25))
     med_rectangle = med_surface.get_rect(
         center=(WIDTH // 2, HEIGHT // 2 + 100))
-    hard_rectangle = hard_surface.get_rect(
-        center=(WIDTH // 2, HEIGHT // 2 + 175))
 
     # Draw buttons
     screen.blit(easy_surface, easy_rectangle)
     screen.blit(med_surface, med_rectangle)
-    screen.blit(hard_surface, hard_rectangle)
 
 
 if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Animal Crossing Town Tune')
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    screen.fill(BG_COLOR)
     draw_start_menu(screen)
+    pygame.display.flip()
+    while True:
+        pygame.time.delay(1000)
+
