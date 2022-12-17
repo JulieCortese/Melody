34f8e5a2f1ce9abc83f9e91a8194fefeb01ea79f
julie <jcortese@ufl.edu>
2022-12-17 18:26:11 -0500

    filled in class Note with functions like class Cell

--- notes.py
+++ notes.py
@@ -1,15 +1,22 @@
 import pygame
 from constants import *
 
+
 class Note:
-    def __init__(self):
-        self.note = 'C'
+    def __init__(self, note, x, y, screen):
+        self.note = note
+        self.x = x
+        self.y = y
+        self.screen = screen
+
+    def set_note_val(self, note):
+        self.note = note
 
     def draw_note(self, screen):
         number_font = pygame.font.Font(None, 60)
-        if self.value == 0:
-            self.value = ''  # changes 0s to empty strings so nothing displays
-        num_surf = number_font.render(str(self.value), 0, (0, 0, 0))
+        if self.note == '-':
+            self.note = ''  # changes 0s to empty strings so nothing displays
+        num_surf = number_font.render(str(self.note), 0, (0, 0, 0))
         chip_rect = num_surf.get_rect(
-            center=(SQUARE_SIZE * self.col + SQUARE_SIZE // 2, SQUARE_SIZE * self.row + SQUARE_SIZE // 2))
+            center=(SQUARE_SIZE * self.y + SQUARE_SIZE // 2, SQUARE_SIZE * self.x + SQUARE_SIZE // 2))
         screen.blit(num_surf, chip_rect)
